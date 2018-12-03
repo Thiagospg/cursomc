93d2519c974a7c1e4220b2767920dd45b0aaa24a
Thiago Gomes <thigomes.ti@gmail.com>
2018-12-03 21:35:42 -0200

    Projeto criado

---
+++ src/main/java/com/thiago/cursomc/CursomcApplication.java
@@ -0,0 +1,14 @@
+package com.thiago.cursomc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.multipart.MultipartFile;
+
+@SpringBootApplication
+public class CursomcApplication {
+
+	public static void main(String[] args) {
+		
+		SpringApplication.run(CursomcApplication.class, args);
+	}
+}
